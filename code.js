2c23ce4a1ab0bd59a2be50713d001224c728ac0c
jlandis320 <jreynoldslandis@gmail.com>
2022-11-18 13:15:22 -0500

    initial commit

---
+++ code.js
@@ -0,0 +1,23 @@
+// * 8kyu
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  //   input is a name
+  //   accepts uppercase and lower -- need to convert to upper
+  //   isolate charat index0
+  //   return string
+    return (name.toUpperCase().charAt(0) === 'R' ? name + " plays banjo" : name + " does not play banjo")
+  //   return name;
+  }
+
+// *-------------------------------------------------------------------------------------------------------*//
+
