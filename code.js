4fd2911016ef7f669e9de979734cbaaa4ac7ef38
jlandis320 <jreynoldslandis@gmail.com>
2022-11-20 15:22:18 -0500

    adds rock paper scissor function

--- code.js
+++ code.js
@@ -40,16 +40,36 @@ function greet(name) {
 // "scissors", "paper" --> "Player 1 won!"
 // "scissors", "rock" --> "Player 2 won!"
 // "paper", "paper" --> "Draw!"
+const rps = (p1, p2) => {
+  //   inputs are strings
+  //   output is strings -- player 1 won, player 2 won, or draw
+  //   if else if else
+    if (p1 === p2){
+      return 'Draw!'
+    } else if (p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' || p1 === 'rock' && p2 === 'scissors'){
+      return 'Player 1 won!'
+    } else {
+        return 'Player 2 won!'
+      }
+  };
 
 // * 8kyu
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
-var countSheep = function (num){
+var countSheep = function (num) {
+  let emptystring = "";
+  let sheep = 1; 
+  for (i = 0; i < num; i++) { 
+    let count = `${sheep++} sheep...`;
+    emptystring = emptystring.concat(count);
+  }
+  return emptystring;
+};
 
-  let emptystring = ''
-  let sheep = 1
-  for (i = 0; i < num; i++){
-    let count = `${sheep++} sheep...`
-    emptystring = emptystring.concat(count)
-    }
-      return emptystring
-}
+// better solve: 
+// var countSheep = function (num){
+//   let str = "";
+//   for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+//   return str;
+// }
+// resolves 47&48 into one line; resolves 50&51 into one line 
+    // remember: 
