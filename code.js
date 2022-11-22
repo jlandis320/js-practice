a864e5ddabb3ad1ef41c7e06ab127ec43920f559
jlandis320 <jreynoldslandis@gmail.com>
2022-11-22 14:25:00 -0500

    adds updateLight function

--- code.js
+++ code.js
@@ -98,3 +98,26 @@ function litres(time) {
   //   return Math.round(litres * time) <- Math.round() will accurately round, Math.floor() will round down
     return Math.floor(time * .5)
   }
+
+// *8kyu
+// Description:
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, when the input is green, output should be yellow.
+
+function updateLight(current) {
+  if (current === 'green') return 'yellow'
+  if (current === 'yellow') return 'red'
+  if (current === 'red') return 'green'
+}
+
+
+// another solve: 
+// function updateLight(current) {
+  
+//   return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+
+// }
